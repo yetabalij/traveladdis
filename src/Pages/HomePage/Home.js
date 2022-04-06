0cb8da05b4b28cb4e7a15efade3a2665c73be1e8
yetabalij <ermiastsegu@yahoo.com>
2022-04-06 21:39:05 +0300

    menu.js and home.js files are created and initialized.

---
+++ src/Pages/HomePage/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Menu from "../../Components/Menu/Menu";
+
+const Home = () => {
+  return (
+    <div>
+        <Menu/>
+      <h1 className="text-3xl font-bold underline">Home</h1>;
+    </div>
+  );
+};
+
+export default Home;
