fa0bd886f421ff76977a4caac554f84b9671a417
yetabalij <ermiastsegu@yahoo.com>
2022-04-07 21:27:08 +0300

    navigation is completed.

--- src/Components/NavItems/NavItems.js
+++ src/Components/NavItems/NavItems.js
@@ -1,7 +1,46 @@
 import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
 
+const NavItemsContainer = styled.div`
+  ${tw`
+       
+    `}
+`;
+const NavItemUl = styled.ul`
+  ${tw`
+    flex
+    
+  `}
+`;
+const NavItem = styled.li`
+  ${tw`
+    px-2
+    text-sm
+    text-primary-color
+    font-medium
+    hover:text-thersiary-color
+  `}
+`;
 const NavItems = () => {
-  return <div>NavItems</div>;
+  return (
+    <NavItemsContainer>
+      <NavItemUl>
+        <NavItem>
+          <a href="#">Home</a>
+        </NavItem>
+        <NavItem>
+          <a href="#">Services</a>
+        </NavItem>
+        <NavItem>
+          <a href="#">About Us</a>
+        </NavItem>
+        <NavItem>
+          <a href="#">Contact Us</a>
+        </NavItem>
+      </NavItemUl>
+    </NavItemsContainer>
+  );
 };
 
 export default NavItems;
