d641cc6ab2ec27e3d2dcdf6fb3a99bb33f44786d
yetabalij <ermiastsegu@yahoo.com>
2022-04-07 20:48:14 +0300

    NabBar component is completed.

--- src/Components/NavBar/NabBar.js
+++ src/Components/NavBar/NabBar.js
@@ -2,10 +2,16 @@ import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 import Brand from "../Brand/Brand";
+import NavItems from "../NavItems/NavItems";
+import ContactUsBtn from "../ContactUsBtn/ContactUsBtn";
 
 const NavBarContainer = styled.div`
   ${tw`
-    bg-red-100
+    w-11/12
+    m-auto
+    flex
+    justify-between
+    my-6
   `}
 `;
 
@@ -13,6 +19,8 @@ const NabBar = () => {
   return (
     <NavBarContainer>
       <Brand />
+      <NavItems />
+      <ContactUsBtn />
     </NavBarContainer>
   );
 };
