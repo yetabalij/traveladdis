d93c029dc2a94bc1a2f72cf4d36d4faead4e38d9
yetabalij <ermiastsegu@yahoo.com>
2022-04-09 19:29:46 +0300

    top section with find destination is structured.

--- src/Pages/HomePage/FindDestination/FindDestination.js
+++ src/Pages/HomePage/FindDestination/FindDestination.js
@@ -1,7 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
 
+const FindDestinationContainer = styled.div`
+  ${tw`
+  
+  `}
+`;
+
+const SectionOne = styled.div`
+  ${tw`
+  `}
+`;
+const SectionTwo = styled.div`
+  ${tw`
+  `}
+`;
+const SectionOneLeft = styled.div`
+  ${tw`
+  `}
+`;
+const SectionOneRight = styled.div`
+  ${tw`
+  `}
+`;
 const FindDestination = () => {
-  return <div>FindDestination</div>;
+  return (
+    <FindDestinationContainer>
+      <SectionOne>
+        <SectionOneLeft>Section one Left</SectionOneLeft>
+        <SectionOneRight>section one Right</SectionOneRight>
+      </SectionOne>
+      <SectionTwo>Section Two</SectionTwo>
+    </FindDestinationContainer>
+  );
 };
 
 export default FindDestination;
