e6c6f189c34d2257902697da662537059845fed3
yetabalij <ermiastsegu@yahoo.com>
2022-04-11 22:17:20 +0300

    popular destination.js is featch data from json file and further beautification and responsive work is left.

--- src/Pages/HomePage/PopularDestination/PopularDestination.js
+++ src/Pages/HomePage/PopularDestination/PopularDestination.js
@@ -1,13 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-// import Hotel2 from "./../../../assets/images/hotel2.png";
-// import Hotel3 from "./../../../assets/images/hotel3.png";
-// import Hotel4 from "./../../../assets/images/hotel4.png";
-// import Hotel5 from "./../../../assets/images/hotel5.png";
-// import Hotel6 from "./../../../assets/images/hotel6.png";
-// import Hotel7 from "./../../../assets/images/hotel7.png";
 import HotelsData from "./../../../data.json";
+import { useState } from "react";
 
 const PopularDestinationContainer = styled.div`
   ${tw`
@@ -75,6 +70,20 @@ const CardImage = styled.div`
   `}
 `;
 const PopularDestination = () => {
+  const hotelsData = HotelsData.map((hotel) => {
+    return (
+      <Card>
+        <CardImage>
+          <img src={hotel.image}></img>
+          <h2>
+            {hotel.city}, {hotel.country}
+          </h2>
+          <h5>{hotel.description}</h5>
+        </CardImage>
+      </Card>
+    );
+  });
+
   return (
     <PopularDestinationContainer>
       <TilteContainer>
@@ -87,30 +96,7 @@ const PopularDestination = () => {
           Our aim is to provide you with a top quality tour exeperiance
         </PopularDestinationText>
       </PopularDestinationTextContainer>
-      <CardContainer>
-        {/* <Card>
-          <CardImage>
-            <img src={Hotel2}></img>
-          </CardImage>
-          <h5>Bangkong</h5>
-        </Card>
-
-        <CardImage>
-          <img src={Hotel3}></img>
-        </CardImage>
-        <CardImage>
-          <img src={Hotel4}></img>
-        </CardImage>
-        <CardImage>
-          <img src={Hotel5}></img>
-        </CardImage>
-        <CardImage>
-          <img src={Hotel6}></img>
-        </CardImage>
-        <CardImage>
-          <img src={Hotel7}></img>
-        </CardImage> */}
-      </CardContainer>
+      <CardContainer>{hotelsData}</CardContainer>
     </PopularDestinationContainer>
   );
 };
