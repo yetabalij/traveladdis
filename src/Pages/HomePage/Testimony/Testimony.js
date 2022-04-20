fc2dd9e28b1395120330875cee09b9d6856cbb8c
yetabalij <ermiastsegu@yahoo.com>
2022-04-20 19:56:12 +0300

    testimony.js component is started.

--- src/Pages/HomePage/Testimony/Testimony.js
+++ src/Pages/HomePage/Testimony/Testimony.js
@@ -1,7 +1,15 @@
 import React from "react";
+import tw from "twin.macro";
+import styled from "styled-components";
 
+const TestimonyContainer = styled.div`
+  ${tw`
+    w-11/12
+    m-auto
+  `}
+`;
 const Testimony = () => {
-  return <div>Testimony</div>;
+  return <TestimonyContainer>Testimony</TestimonyContainer>;
 };
 
 export default Testimony;
