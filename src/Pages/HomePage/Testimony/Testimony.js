911b9d8264a4b56ef9eb36c19e41cbe734accc3b
yetabalij <ermiastsegu@yahoo.com>
2022-04-20 22:24:41 +0300

    slide component images are added.

--- src/Pages/HomePage/Testimony/Testimony.js
+++ src/Pages/HomePage/Testimony/Testimony.js
@@ -2,6 +2,33 @@ import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 
+import Customer1 from "./../../../assets/images/customer1.png";
+import Customer2 from "./../../../assets/images/customer2.png";
+import Customer3 from "./../../../assets/images/customer3.png";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
 const TestimonyContainer = styled.div`
   ${tw`
     w-11/12
@@ -40,8 +67,22 @@ const SliderContainer = styled.div`
   
   `}
 `;
+const CardContainer = styled.div`
+  ${tw`
+    flex
+    justify-center
+  `}
+`;
+const CardImage = styled.div`
+  ${tw`
+  
+  `}
+  img {
+  }
+`;
 
-const Testimony = () => {
+const Testimony = (props) => {
+  console.log(responsive.mobile);
   return (
     <TestimonyContainer>
       <TitleTextContainer>
@@ -50,7 +91,52 @@ const Testimony = () => {
         </TitleUncolored>
       </TitleTextContainer>
       <SliderContainer>
-        
+        <Carousel
+          //swipeable={false}
+          //draggable={false}
+          //showDots={true}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          //autoPlay={this.props.deviceType !== "mobile" ? true : false}
+          autoPlaySpeed={1000}
+          //keyBoardControl={true}
+          //customTransition="all .5"
+          transitionDuration={1000}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          //deviceType={this.props.deviceType}
+          //dotListClass="custom-dot-list-style"
+          //itemClass="carousel-item-padding-40-px"
+          //slidesToSlide={4}
+          //arrows={false}
+        >
+          <CardContainer>
+            <CardImage>
+              <img src={Customer1} />
+            </CardImage>
+          </CardContainer>
+          <CardContainer>
+            <CardImage>
+              <img src={Customer2} />
+            </CardImage>
+          </CardContainer>
+          <CardContainer>
+            <CardImage>
+              <img src={Customer3} />
+            </CardImage>
+          </CardContainer>
+          <CardContainer>
+            <CardImage>
+              <img src={Customer1} />
+            </CardImage>
+          </CardContainer>
+          <CardContainer>
+            <CardImage>
+              <img src={Customer2} />
+            </CardImage>
+          </CardContainer>
+        </Carousel>
       </SliderContainer>
     </TestimonyContainer>
   );
