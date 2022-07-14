af2d2647738ead5cd25315179da7bc3367ec66de
yetabalij <ermiastsegu@yahoo.com>
2022-07-14 14:08:18 +0300

    strock color changed.

--- src/Components/MobNav/MobNav.js
+++ src/Components/MobNav/MobNav.js
@@ -30,9 +30,10 @@ const MobNav = (props) => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              stroke-width="4"
             >
               <path
+                stroke="white"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 d="M6 18L18 6M6 6l12 12"
