73891d2eb92922e2962a2cc4155eb720401c853a
yetabalij <ermiastsegu@yahoo.com>
2022-07-14 16:09:15 +0300

    content modified.

--- src/Pages/HomePage/Works/Works.js
+++ src/Pages/HomePage/Works/Works.js
@@ -4,14 +4,12 @@ import tw from "twin.macro";
 import Add from "./../../../assets/images/Add.png";
 import Search from "./../../../assets/images/Search.png";
 import Send from "./../../../assets/images/Send.png";
-import Path1 from "./../../../assets/images/Path1.png";
-import Path2 from "./../../../assets/images/Path2.png";
 
 const WorkContainer = styled.div`
   ${tw`
     w-11/12
     m-auto
-    mb-10
+    mb-16
     sm:mb-8
   `}
 `;
@@ -53,61 +51,13 @@ const WorksContainer = styled.div`
     sm:mt-20
   `}
 `;
-const SectionOne = styled.div`
-  ${tw`
-    flex
-    justify-center
-    items-start
-    overflow-x-hidden
-  `}
-`;
+
 const ImageContainer = styled.div`
   ${tw`
-    
+    mb-3
   `}
-  img {
-    width: 70%;
-    height: 100%;
-  }
 `;
-const ImageContainer2 = styled.div`
-  ${tw`
-    
-  `}
-  img {
-  }
-`;
-const ImageContainer3 = styled.div`
-  ${tw`
-    mt-10
-    lg:mt-20
-    lg:pl-5
-  `}
-  img {
-    width: 70%;
-    height: 100%;
-  }
-`;
-const ImageContainer4 = styled.div`
-  ${tw`
-    mt-12
-    lg:mt-16
-    overflow-hidden
-  `}
-  img {
-    transform: rotate(-10deg);
-  }
-`;
-const ImageContainer5 = styled.div`
-  ${tw`
-    mt-16
-    lg:mt-20
-  `}
-  img {
-    width: 70%;
-    height: 100%;
-  }
-`;
+
 const SectionTwo = styled.div`
   ${tw`
     sm:flex
@@ -187,25 +137,11 @@ const Works = () => {
         </HowItWorksUncolord>
       </HowItWorksContainer>
       <WorksContainer>
-        <SectionOne>
-          <ImageContainer>
-            <img alt="" src={Add}></img>
-          </ImageContainer>
-          <ImageContainer2>
-            <img alt="" src={Path1}></img>
-          </ImageContainer2>
-          <ImageContainer3>
-            <img alt="" src={Search}></img>
-          </ImageContainer3>
-          <ImageContainer4>
-            <img alt="" src={Path2}></img>
-          </ImageContainer4>
-          <ImageContainer5>
-            <img alt="" src={Send}></img>
-          </ImageContainer5>
-        </SectionOne>
         <SectionTwo>
           <SingUpContainer>
+            <ImageContainer>
+              <img className="w-1/6" alt="" src={Add}></img>
+            </ImageContainer>
             <SignUpTitle>Sign Up</SignUpTitle>
             <SignUpText>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
@@ -214,6 +150,9 @@ const Works = () => {
             </SignUpText>
           </SingUpContainer>
           <SearchContainer>
+            <ImageContainer>
+              <img className="w-1/6" alt="" src={Search}></img>
+            </ImageContainer>
             <SearchTitle>Search</SearchTitle>
             <SearchText>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
@@ -222,6 +161,9 @@ const Works = () => {
             </SearchText>
           </SearchContainer>
           <SendContainer>
+            <ImageContainer>
+              <img className="w-1/6" alt="" src={Send}></img>
+            </ImageContainer>
             <SendTitle>Travel</SendTitle>
             <SendText>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
