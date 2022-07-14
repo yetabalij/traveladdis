2a2f4d389d14a629f9416ecdb9085b18226d8ec9
yetabalij <ermiastsegu@yahoo.com>
2022-07-14 14:37:02 +0300

    id added.

--- src/Pages/HomePage/Testimony/Testimony.js
+++ src/Pages/HomePage/Testimony/Testimony.js
@@ -135,7 +135,7 @@ const Paragraph = styled.p`
 const Testimony = (props) => {
   console.log(responsive.mobile);
   return (
-    <TestimonyContainer>
+    <TestimonyContainer id="testmony">
       <TitleTextContainer>
         <TitleUncolored>
           What Our Happy <TitleColored>Clients </TitleColored>Say?
@@ -143,24 +143,13 @@ const Testimony = (props) => {
       </TitleTextContainer>
       <SliderContainer>
         <Carousel
-          //swipeable={false}
-          //draggable={false}
-          //showDots={true}
           responsive={responsive}
           ssr={true} // means to render carousel on server-side.
           infinite={true}
-          //autoPlay={this.props.deviceType !== "mobile" ? true : false}
           autoPlaySpeed={1000}
-          //keyBoardControl={true}
-          //customTransition="all .5"
           transitionDuration={1000}
           containerClass="carousel-container"
           removeArrowOnDeviceType={["tablet", "mobile"]}
-          //deviceType={this.props.deviceType}
-          //dotListClass="custom-dot-list-style"
-          //itemClass="carousel-item-padding-40-px"
-          //slidesToSlide={4}
-          //arrows={false}
         >
           <CardContainer>
             <CardImage>
